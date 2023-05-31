b7ce2bd86fdddc816d912f13886ef9869f7f3f06
Kaganovich <JKaganovich@jjsnack.com>
2023-05-31 17:26:12 -0400

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<nav class="tv-static">
+	<a href="/">home</a>
+	<a href="/blog">blog</a>
+	<a>socials</a>
+</nav>
+
+<slot />
+
+<style>
+	nav {
+		text-align: center;
+		/* make nav height equal to its inside contents */
+		display: inline-flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+	a {
+		margin: 0 5px;
+		box-shadow: 3px 3px 0 0 #00ffff;
+		padding: 5px 10px;
+		text-decoration: none;
+		color: #00ffff;
+		border: 1px solid #00ffff;
+		transition: all 0.2s ease-in-out;
+	}
+
+	a:hover {
+		box-shadow: none;
+		font-weight: bold;
+		background-color: #00ffff;
+		color: #000000;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<img src="avatar.png" alt="Jon v2.0" />
+
+<p>Jon v2.0</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+	p {
+		text-align: center;
+		font-size: 1.5rem;
+	}
+
+	img {
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
+		width: 250px;
+	}
+</style>
